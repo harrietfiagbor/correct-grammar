5a9c7fab9453f6a2e048cd0ed8911c41500eeb15
harrietfiagbor <harrietfiagbor@gmail.com>
2023-08-21 19:39:33 +0000

    add models

--- src/correct.py
+++ src/correct.py
@@ -1,20 +1,42 @@
 import argparse
-from spellchecker import SpellChecker
+import os 
+import yaml 
+import torch
+from torch import package
+from ftlangdetect import detect
+import pathlib
 
-parser = arparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 parser.add_argument("--file-path", default='./sample-docs/text-1.txt', type=pathlib.Path )
 parser.add_argument("--lang", default="en")
+parser.add_argument("--debug", action="store_true")
 args = parser.parse_args()
+
+if not os.path.isfile('latest_silero_models.yml'):
+    torch.hub.download_url_to_file('https://raw.githubusercontent.com/snakers4/silero-models/master/models.yml',
+                                   'latest_silero_models.yml', 
+                                   progress=False) 
+
+with open('latest_silero_models.yml') as model_file:
+    models = yaml.load(model_file, Loader=yaml.SafeLoader)
+models_conf = models.get("te_models").get("latest")
+
 
 with open(args.file_path, 'r') as file:
     input_text = file.read()
 
-print(f"Unformatted text {}".format(input_text))
+print("--------------- Unformatted text -----------------\n{}".format(input_text))
 
-if args.lang == "detect":
-    lang = detect(text=input_text, low_memory=False)['lang']
-    print(f"Detected language, {lang}")
+lang = args.lang
 
-check_spelling = SpellChecker(language=lang)
+if lang == "detect":
+    print('Detecting language...')
+    lang = detect(text=input_text.replace('\n',''), low_memory=True)['lang']
+    print(f"Language detected: {lang.title()}")
+
+if args.debug:
+  pass 
+   
+
 
 
